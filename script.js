7a6b28aa8a914a8f7315333bfbfdba54fab7c7c7
Thennavan-Hex <thensthennavan3@gmail.com>
2023-06-14 21:41:27 +0530

    new file added

---
+++ script.js
@@ -0,0 +1,32 @@
+function handleSmallScreens() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarMenu = document.querySelector('.navbar-menu');
+
+    navbarToggler.addEventListener('click', () => {
+      if (!navbarMenu.classList.contains('active')) {
+        navbarMenu.classList.add('active');
+        // Scroll down when mobile menu is active
+        window.scrollTo(0, document.body.scrollHeight);
+      } else {
+        navbarMenu.classList.remove('active');
+      }
+    });
+  }
+
+  handleSmallScreens();
+  // Toggle the light and dark theme
+function toggleTheme() {
+    const body = document.querySelector('body');
+    const navbar = document.querySelector('.navbar');
+    const themeToggleButton = document.getElementById('theme-toggle-btn');
+
+    body.classList.toggle('light-mode');
+    body.classList.toggle('dark-mode');
+    navbar.classList.toggle('light-mode');
+    navbar.classList.toggle('dark-mode');
+    themeToggleButton.classList.toggle('active');
+}
+
+// Add event listener to the theme toggle button
+const themeToggleButton = document.getElementById('theme-toggle-btn');
+themeToggleButton.addEventListener('click', toggleTheme);
